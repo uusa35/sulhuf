f67b88c63e93a8d5c9b7299dd78514641c06e17c
uusa35 <uusa35@gmail.com>
2022-07-16 14:35:17 +0200

    cleaning up

--- components/layout/MainLayout.tsx
+++ components/layout/MainLayout.tsx
@@ -76,39 +76,29 @@ const MainLayout = ({children}: MainLayoutProps) => {
                 <title>Sulhuf</title>
                 <link rel="icon" href="/sulhuf.png"/>
             </Head>
-            <div className="h-screen w-full font-tajwal-medium capitalize" dir={locale.isRTL ? 'rtl' : 'ltr'}>
+            <div className="min-h-screen w-full font-tajwal-medium capitalize" dir={locale.isRTL ? 'rtl' : 'ltr'}>
                 <MainNav />
-
-                <div className="py-10">
                     <header>
-                        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-10">
                             <h1 className="text-3xl font-bold leading-tight text-gray-900">SULHUF</h1>
                         </div>
                     </header>
-                    <main className={`mx-auto mt-12`}>
-                        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8  h-full ">
-                            {/* Replace with your content */}
-                            <div className="px-4 py-8 sm:px-0">
-                            </div>
-                            <div className={``}>
+                    <main className={`mx-auto`}>
+                        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                                 {children}
-                            </div>
                         </div>
+                        <footer
+                            className="flex items-center justify-center w-full border-t absolute bottom-0 border-t shadow-lg p-3">
+                            <Link
+                                as={`home`}
+                                className="flex items-center justify-center gap-2"
+                                href="/"
+                            >
+                                <Image src="/sulhuf.png" alt="Vercel Logo" width={120} height={40}/>
+                            </Link>
+                        </footer>
                     </main>
-                </div>
-                <footer
-                    className="flex items-center justify-center w-full border-t absolute bottom-0 border-t shadow-lg p-3">
-                    <Link
-                        as={`home`}
-                        className="flex items-center justify-center gap-2"
-                        href="/"
-                    >
-                        <Image src="/sulhuf.png" alt="Vercel Logo" width={120} height={40}/>
-                    </Link>
-                </footer>
             </div>
-
-
             <ToastContainer />
         </MainContext.Provider>
     );
